54db597fbd47ca94d0433f9e545a0fe766a614e1
Aliwoto <aminnimaj@gmail.com>
2021-07-08 09:01:38 +0000

    Added versioning, wotoStrings, wotoStrongs packages
    Signed-off-by: Aliwoto <aminnimaj@gmail.com>

--- main.go
+++ main.go
@@ -2,34 +2,30 @@ package main
 
 import (
 	"log"
-	"net/http"
 	"os"
+
+	"ltw-server/wotoPacks/entryPoints"
 
 	"github.com/gin-gonic/gin"
 )
-
-var _count int
 
 func main() {
 	port := os.Getenv("PORT")
 
 	if port == "" {
-		log.Fatal("$PORT must be set")
+		port = "8080"
 	}
 	log.Println("we are here!" + port)
-	//wotoPacks.ServerDaytime(port)
 
 	router := gin.New()
 	router.Use(gin.Logger())
 	//router.LoadHTMLGlob("templates/*.tmpl.html")
 	//router.Static("/static", "static")
 
-	router.GET("/", func(c *gin.Context) {
-		//c.HTML(http.StatusOK, "index.tmpl.html", nil)
-		_s := c.Request.Header.Get("test")
-		_count++
-		c.String(http.StatusOK, "%v %v; counte: %v", "yes!", _s, _count)
-	})
+	err := entryPoints.LoadEntryPoints(router)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	router.Run(":" + port)
 }
